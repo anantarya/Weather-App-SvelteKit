8e4c78b0bfee72f457a6afa2e08b1875eff64b86
ANANT <anantking11@gmail.com>
2024-03-04 22:54:25 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<section>
+    Input a city in the search and get the current weather from the <a
+        href="https://openweathermap.org/"
+        target="_blank">OpenWeather API</a
+    >.
+</section>
+
+<style>
+    section {
+        text-align: center;
+    }
+</style>
